8d7d1888e89143e803c207802c97c450e26bef8b
Idan Nikritin <idan.nik@gmail.com>
2019-10-14 08:48:04 +0300

    Creating classes State, FSM, rewriting tests

--- fsm.py
+++ fsm.py
@@ -1,61 +1,21 @@
-def START():
-    return LISTENING
+class State:
+    def __init__(self, name, events, default_event='ERROR'):
+        self.name = name
+        self.events = events
+        self.default_event = default_event
+
+    def apply(self, event):
+        return self.events.get(event, self.default_event)
 
 
-def LISTENING(event):
-    if event == "connect":
-        return CONNECTED
-    elif event == "error":
-        return LISTENING
-    else:
-        return ERROR
+class FSM:
+    def __init__(self, states, start):
+        self.states = {s.name: s for s in states}
+        self.start_state = self.states[start]
 
-
-def CONNECTED(event):
-    if event == "accept":
-        return ACCEPTED
-    elif event == "close":
-        return CLOSED
-    else:
-        return ERROR
-
-
-def ACCEPTED(event):
-    if event == "close":
-        return CLOSED
-    elif event == "read":
-        return READING(event)
-    elif event == "write":
-        return WRITING(event)
-    else:
-        return ERROR
-
-
-def READING(event):
-    if event == "read":
-        return READING
-    elif event == "write":
-        return WRITING(event)
-    elif event == "close":
-        return CLOSED
-    else:
-        return ERROR
-
-
-def WRITING(event):
-    if event == "read":
-        return READING(event)
-    elif event == "write":
-        return WRITING
-    elif event == "close":
-        return CLOSED
-    else:
-        return ERROR
-
-
-def CLOSED(event):
-    return LISTENING(event)
-
-
-def ERROR(event):
-    return ERROR
+    def get_state_by_events(self, lst):
+        state = self.start_state
+        for event in lst:
+            next = state.apply(event)
+            state = self.states[next]
+        return state.name
--- test_fsm.py
+++ test_fsm.py
@@ -1,10 +1,45 @@
 import unittest
 
+from fsm import State, FSM
+
+CONNECTION_STATES = [
+    State('LISTENING', {'connect': 'CONNECTED',
+                        'error': 'LISTENING'
+                        }),
+    State('CONNECTED', {
+        'accept': 'ACCEPTED',
+        'close': 'CLOSED'
+    }),
+    State('ACCEPTED', {'close': 'CLOSED',
+                       'read': 'READING',
+                       'write': 'WRITING'}),
+    State('READING', {'read': 'READING',
+                      'close': 'CLOSED',
+                      'write': 'WRITING'}),
+    State('WRITING', {'read': 'READING',
+                      'close': 'CLOSED',
+                      'write': 'WRITING'}),
+    State('CLOSED', {}, default_event='LISTENING'),
+    State('ERROR', {}, default_event='ERROR')
+]
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.fsm = FSM(CONNECTION_STATES, start='LISTENING')
 
+
+    def test_connect_accept_close(self):
+        assert self.fsm.get_state_by_events(['connect', 'accept', 'read', 'close']) == 'CLOSED'
+
+    def test_read_before_connect_raise_error(self):
+        assert self.fsm.get_state_by_events(['read', 'connect', 'accept', 'read', 'close']) == 'ERROR'
+
+    def test_read_read_read(self):
+        assert self.fsm.get_state_by_events(['connect', 'accept', 'read', 'read', 'read']) == 'READING'
+
+    def test_write_write_write(self):
+        assert self.fsm.get_state_by_events(['connect', 'accept', 'write', 'write', 'write']) == 'WRITING'
 
 if __name__ == '__main__':
     unittest.main()
